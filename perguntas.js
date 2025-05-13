00202a220657f1e3ba5359e18597b54904dda611
ediandreska <edineia.andreska@escola.pr.gov.br>
2025-05-13 14:28:18 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'programação'
+    'o que é HTML',
+    linguagem de marcação
+)
+
+criaCartao(
+    categoria
+    pergunta,
+    resposta
+
+
+    criaCartao(
+    categoria
+    pergunta,
+    resposta
