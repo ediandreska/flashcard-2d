86ede15247d41fb39eba8bf34a924cb48b0f9a30
ediandreska <edineia.andreska@escola.pr.gov.br>
2025-06-17 14:29:56 +0000

    ok

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ function criaCartao(categoria,pergunta,resposta){
    let container = document.getElementById('container')
    let cartao = document.createElement('article')
    cartao.className = 'cartao'
-   cartao.innerHTML = `            <div class="cartao__conteudo">
+   cartao.innerHTML = `            
+   <article cl
+   <div class="cartao__conteudo">
                 <h3>programação</h3>
                 <div class="cartao__conteudo__pergunta">
                     <h3> {categoria} </h3>
@@ -10,7 +12,14 @@ function criaCartao(categoria,pergunta,resposta){
                 <div class="cartao__conteudo__resposta">
                   <p>linguagem de programação</p>  
                 </div>`
-                </div>
-                `
+              
+    </article>            `
             
-    
+  </article>
+
+
+  let respostaEstaVIsivel= false
+
+  function viraCartao () {
+  respostaEstaVisivel = !respostaEstaVisivel
+  cartao.}
